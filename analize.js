29608331694e4cd7898ff4d25c05af15fb5e1bd1
김지섭 <0226daniel@gmail.com>
2018-05-26 01:38:58 +0900

    연동 성공

--- analize.js
+++ analize.js
@@ -1,19 +1,50 @@
-inlets = 1;
-setinletassist(0,"midiin");
-/*setinletassist(0,"Note-on and Note-off (list: Pitch, Velocity)");
-setinletassist(1,"Poly Key Pressure (list: KEY, Value)");
-setinletassist(2,"Control Change (list: Controller Number, Value)");
-setinletassist(3,"Program Change");
-setinletassist(4,"Aftertouch");
-setinletassist(5,"Pitch Bend (0-127)");
-setinletassist(6,"MIDI Channel");*/
+inlets = 4
+setinletassist(0,"Note-on and Note-off (Pitch)")
+setinletassist(1,"Note-on and Note-off (Velocity)")
+setinletassist(2,"Control Change (Controller Number)")
+setinletassist(3,"Control Change (Value)")
 
-outlets = 3;
-setoutletassist(0,"onPadTouch");
-setoutletassist(1,"onFunctionkeyTouch");
-setoutletassist(2,"onChainTouch");
+outlets = 3
+setoutletassist(0,"onPadTouch")
+setoutletassist(1,"onFunctionkeyTouch")
+setoutletassist(2,"onChainTouch")
 
-function msg_int(v) {
-	;
-	outlet(0, "f"+ midiparse(v))
+var v = new Array()
+v[0] = v[1] = v[2] = v[3] = 0
+var noteSig = 0
+var controlSig = 0
+
+function msg_int(i) {
+	v[inlet] = i
+	switch(inlet){
+		case 0:
+		case 1:
+			noteSig += 1
+			break;
+		case 2:
+		case 3:
+			controlSig += 1
+			break;
+	}
+
+	if(noteSig >= 2)
+		onNote()
+	if(controlSig >= 2)
+		onControl()
+}
+
+function onNote() {
+	noteSig = 0
+	var note = v[0]
+	var velo = v[1]
+
+	var x = 9 - Math.floor(note / 10);
+	var y = note % 10;
+	
+	if (y >= 1 && y <= 8)
+		outlet(0, (x - 1) + " / " + (y - 1) + " / " + (velo != 0))
+}
+function onControl() {
+	controlSig = 0;
+	outlet(1, v[2] + " / " + v[3]);
 }
